0c82fbdedb515ca9e5f22cdb03f37c62e8287251
Jacob Medchill <medch004@umn.edu>
2023-03-02 08:15:50 -0600

    get_test_ecgs function within get_ECG_from_DB.py adds the functionality of getting random ecgs from the database

--- get_ECG_from_DB.py
+++ get_ECG_from_DB.py
@@ -1,12 +1,9 @@
-import random
-
 from sqlite_setup import get_sqlite_connection
-from random import seed
-from random import randrange
+import random, math
 import csv
 
 #Argument: arrhythmia IDs (array of ints)
-def get_test_ecgs(idArray, numEcg):
+def get_train_ecgs(idArray, numEcg):
     numArrhy = len(idArray)
     if(type(idArray) != list):
         print("get_test_ecgs: Invalid argument. idArray must by of type list")
@@ -19,10 +16,14 @@ def get_test_ecgs(idArray, numEcg):
         return
 
     random.seed()
-    #Decide number of ecgs for each arrhythmia
-    arrhyCount = [1] * numArrhy #Make sure each arrhythmia has at least one ecg
-    for i in range(numEcg - numArrhy):
+    # Decide number of ECGs for each arrhythmia
+    # Calculate the number of ECGs each arrhythmia can have while keeping below the requested number of ECGs
+    numEqualEcgs = math.floor(numEcg / numArrhy)
+    arrhyCount = [numEqualEcgs] * numArrhy #Make sure each arrhythmia has at least one ecg
+    for i in range(numEcg - numEqualEcgs * numArrhy):
         tempInd = random.randrange(0, numArrhy)
+        while arrhyCount[tempInd] > numEqualEcgs:
+            tempInd = random.randrange(0, numArrhy)
         arrhyCount[tempInd] = arrhyCount[tempInd] + 1
     #Set up return array
     #returnedEcgs is a list where the ecgs stored at the index i have the arrhythmia which was given at index i of
@@ -36,6 +37,47 @@ def get_test_ecgs(idArray, numEcg):
     cur = con.cursor()
     for i in range(numArrhy): #For each type of arrhythmia given
         for j in range(arrhyCount[i]): #For each ecg that will be fetched for a particular arrhythmia
+            res = cur.execute(
+                "SELECT ecg.graph "
+                "FROM ecg, diagnosis "
+                "WHERE ecg.id = diagnosis.ecg_id "
+                    "AND diagnosis.arrhythmia_id = " + str(idArray[i]) + " "
+                "ORDER BY RANDOM() "
+                "LIMIT 1"
+            )
+            returnedEcgs[i][j] = res.fetchone()
+    return returnedEcgs
+
+def get_test_ecgs(idArray, numEcg):
+    numArrhy = len(idArray)
+    if(type(idArray) != list):
+        print("get_train_ecgs: Invalid argument. idArray must by of type list")
+        return
+    elif(type(numEcg) != int):
+        print("get_train_ecgs: Invalid argument. numEcg must by of type int")
+        return
+    elif(numEcg < numArrhy):
+        print("get_train_ecgs: Invalid argument. numEcg >= len(idArray) must be true")
+        return
+
+    random.seed()
+    #Decide number of ecgs for each arrhythmia
+    arrhyCount = [1] * numArrhy #Make sure each arrhythmia has at least one ecg
+    for i in range(numEcg - numArrhy):
+        tempInd = random.randrange(0, numArrhy)
+        arrhyCount[tempInd] = arrhyCount[tempInd] + 1
+    # Set up return array
+    # returnedEcgs is a list where the ecgs stored at the index i have the arrhythmia which was given at index i of
+    # idArray. Each element at index i of returnedEcgs is a list of length equal to the value of arrhyCount[i]. Each of
+    # the elements within returnedEcgs[i] is an ecg with the arrhythmia given at idArray[i].
+    returnedEcgs = [0] * numArrhy
+    for i in range(numArrhy):
+        returnedEcgs[i] = [0] * arrhyCount[i]
+    # Get ecgs from database
+    con = get_sqlite_connection()
+    cur = con.cursor()
+    for i in range(numArrhy): # For each type of arrhythmia given
+        for j in range(arrhyCount[i]): # For each ecg that will be fetched for a particular arrhythmia
             res = cur.execute(
                 "SELECT ecg.graph "
                 "FROM ecg, diagnosis "
