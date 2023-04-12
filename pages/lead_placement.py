124c274640f192c2e79a4a60edd2eeaa46547249
NERYS\mcfra <fran0716@umn.edu>
2023-04-11 22:16:33 -0500

    second round of updates to lead placement page

--- pages/lead_placement.py
+++ pages/lead_placement.py
@@ -13,9 +13,10 @@ class LeadPlacement(QWidget):
         super().__init__()
 
         self.layout = QVBoxLayout(self)
-        self.layout.addWidget(HeadingLabel("Where are the 12 Leads Placed on the Body"))
+        self.layout.addWidget(HeadingLabel("Where are the 12 Leads Placed on the Body?"))
 
         layout2 = QHBoxLayout()
+        layout3 = QHBoxLayout()
         self.lead_placement_explanation = ResizingTextEdit()
         self.lead_placement_explanation.setReadOnly(True)
 
@@ -38,17 +39,17 @@ class LeadPlacement(QWidget):
             <ul>
                 <li>Lead I</li>
                 <ul>
-                    <li>RA (-) to LA (=)</li>
+                    <li>RA (-) to LA (+)</li>
                 </ul>
         
                 <li>Lead II</li>
                 <ul>
-                    <li>RA (-) to LF (+)</li>
+                    <li>RA (-) to LL (+)</li>
                 </ul>
         
                 <li>Lead III</li>
                 <ul>
-                    <li>LA (-) to LF (+)</li>
+                    <li>LA (-) to LL (+)</li>
                 </ul>
             </ul>
         
@@ -56,17 +57,17 @@ class LeadPlacement(QWidget):
             <ul>
                 <li>Lead aVR</li>
                 <ul>
-                    <li>RA (-) to (LA & LF) (+)</li>
+                    <li>RA (-) to (LA & LL) (+)</li>
                 </ul>
         
                 <li>Lead aVL</li>
                 <ul>
-                    <li>LA (+) to (RA & LF) (-)</li>
+                    <li>LA (+) to (RA & LL) (-)</li>
                 </ul>
         
                 <li>Lead aVF</li>
                 <ul>
-                    <li>LF (+) to (LA & RA) (-)</li>
+                    <li>LL (+) to (LA & RA) (-)</li>
                 </ul>
             </ul>  
         
@@ -87,6 +88,7 @@ class LeadPlacement(QWidget):
                 </ul>
             </ul>
 
+            <br>
             <li> Another way to relate these different perspectives together is to look at how they relate anatomically:</li>
             <ul>
                 <li>Leads II, III, and aVF relate to the inferior surface of the heart.</li>
@@ -96,31 +98,49 @@ class LeadPlacement(QWidget):
                 </ul>
             </ul>
 
+            <br>
             <li>*Limb leads are said to "view" the heart along the vertical plane of the chest</li>
             <li>**Chest leads are said to "view" the heart along the horizontal plane of the chest</li>
             <li>***Midaxillary line is defined as the imaginary line that extends down from the middle of the armpit</li>
             </ul>
 
+            <br>
             <li> Sources:</li>
             <li>1) Chatterjee, S. and Miller, A. "Biomedical Instrumentation Systems." Delmar, Cengage Learning. 2010.</li>
             <li>2) Malminvuo, J. and Plonsey, R. "Bioelectromagnetism - Principles and Applications of Bioelectric and Biomagnetic Fields." Oxford University Press. 1995.</li>
             <li>3) Meek, S. and Morris, F. "ABC of clinical electrocaridography: Introduction. I-Leads, rate, rhythm, and cardiac axis." BMJ Clinical Review. Vol 324: 415-8. 16FEB2002.<li>
+            <li>4) Madhero88, ECGcolor.svg, 2009, Retrieved from: https://commons.wikimedia.org/wiki/File:ECGcolor.svg. On April 11, 2023.<li>
             </ul>
         </ul>
         """)
 
-        self.layout.setAlignment(PyQt6.QtCore.Qt.AlignmentFlag.AlignLeft)
-
+        self.layout.setAlignment(PyQt6.QtCore.Qt.AlignmentFlag.AlignTop)
         image = ImageWidget(True)
         pixmap = QPixmap('images:lead_placement.png')
         image.setPixmap(pixmap)
-        image.setMaximumHeight(800)
+        image.setMaximumHeight(500)
 
         layout2.addWidget(self.lead_placement_explanation)
         layout2.addWidget(image)
-        layout2.setAlignment(image, PyQt6.QtCore.Qt.AlignmentFlag.AlignTop)
+        layout2.setAlignment(PyQt6.QtCore.Qt.AlignmentFlag.AlignTop)
+        layout2.setSpacing(50)
 
         layout2.setStretch(0, 1)
         layout2.setStretch(1, 1)
 
         self.layout.addLayout(layout2)
+
+        self.layout.setAlignment(PyQt6.QtCore.Qt.AlignmentFlag.AlignBottom)
+        image2 = ImageWidget(True)
+        pixmap2 = QPixmap('images:Annoatated_ECGcolor.png')
+        image2.setPixmap(pixmap2)
+        image2.setMaximumHeight(1000)
+
+        layout3.addWidget(self.lead_placement_explanation)
+        layout3.addWidget(image2)
+        layout3.setAlignment(image2, PyQt6.QtCore.Qt.AlignmentFlag.AlignBottom)
+       
+        layout3.setStretch(0, 1)
+        layout3.setStretch(1, 1)
+
+        self.layout.addLayout(layout3)
